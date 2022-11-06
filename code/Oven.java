d09d560143132330dbc9c90d8d38844ae020f1d5
Kevin Li <54922253+kevinli22527@users.noreply.github.com>
2022-11-06 12:47:03 -0600

    defined requirements and started designing

--- code/Oven.java
+++ code/Oven.java
@@ -1,43 +1,73 @@
 package code;
 
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+//SLDC STEP 1 : STAKEHOLDER REQUIREMENTS
+/*
+This oven represents a smart oven.
+It needs a heating element with associated status
+Needs a way to control the oven's heating cycle - can be controlled manually or automatically
+manual - set it for longer than automatic will allow
+automatic - set for 120 minutes
+once the timer hits 0, then the heating element will turn off
+need a command console to control the oven
+commands supported:
+"START" - starts the oven
+"STOP" - manually stops the oven and clears the timer (defaults back to 120 min)
+"SET TIME <time>" - sets the timer to the specified time, time is in minutes
+	for example "SET TIME 200"
+"QUIT"	- exits the program
+*/
+
+//SLDC STEP 2 : DESIGN
 public class Oven {
-	private int temperature;  //target temperature in F
-	private int realTemperature;
-	private boolean lightstatus;
-	
-	public static void readMe() {
-		System.out.println("Hi this is a program for ovens");
+	boolean heatingElementOn = false;
+	String name;
+	Timer timer = new Timer();
+	int remainingTime = 120;
+
+	//constructor
+	public Oven(String name) {
+		this.name = name;
+		timer.cancel(); //make sure that when oven starts, the timer isn't counting
 	}
-	
-	public Oven() {
-		this.temperature = 75;
-		this.lightstatus = false; //off
+
+	//starts the oven and the timer countdown
+	public void start() {
+		heatingElementOn = true;
+		System.out.println("Oven is on");
 	}
-	
-	/**
-	 * toggles the light
-	 */
-	public void toggleLight() {
-		this.lightstatus = !this.lightstatus;
+
+	//sets the timer to a manual value
+	public void setTimer(int time) {
+		remainingTime = time;
+		System.out.println("Timer set to " + time + " minutes");
 	}
-	
-	/*
-	 * User puts in the temperature, and the temperature would try and reach the setting
-	 */
-	public void setTemperature(int newTemp) {
-		if (this.lightstatus == true) {
-			this.temperature = newTemp;
-		}
+
+	//returns the remaining time on the timer
+	public int getRemainingTime() {
+		reuturn time;
 	}
-	
-	@Override
+
+	//stops the oven and clears the timer
+	public void stop() {
+		heatingElementOn = false;
+		System.out.println("Oven manual shutoff");
+		remainingTime = 120;
+	}
+
+	//when the timer reaches 0, we call this method to turn off the heating element and notify the user that the heating is done
+	public void processTimerUp() {
+		heatingElementOn = false;
+		System.out.println("Heating is done");
+		remainingTime = 120;
+	}
+
+	//toString
 	public String toString() {
-		String lightStatusString;
-		if (this.lightstatus == false) {
-			lightStatusString = "OFF";
-		} else {
-			lightStatusString = "ON";
-		}
-		return Integer.toString(this.temperature) + " " + lightStatusString;
+		return "Oven [heatingElementOn=" + heatingElementOn + ", name=" + name + ", remainingTime="
+				+ remainingTime + "]";
 	}
 }
--- code/YouCanRunThis.java
+++ code/YouCanRunThis.java
@@ -1,15 +1,30 @@
 package code;
+import java.util.Scanner;
 
 public class YouCanRunThis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Oven myOven = new Oven();
-		Oven kevinOven = new Oven();
-		myOven.toggleLight();
-//		myOven.toggleLight();
-		System.out.println(myOven);
-		System.out.println(kevinOven);
+		Scanner kb = new Scanner(System.in);
+		Oven myOven = new Oven("My Oven");
+		while (true):
+			//process commands
+			String command = kb.nextLine();
+			String[] com_parts = command.split(" ");
+			if (com_parts[0].equals("START")) {
+				//start the oven
+				myOven.start();
+			} else if (com_parts[0].equals("STOP")) {
+				//stop the oven
+				myOven.stop();
+			} else if (com_parts[0].equals("QUIT")) {
+				break;
+			} else if (com_parts[0].equals("SET") && com_parts[1].equals("TIME") && com_parts.length == 3) {
+				//set the timer
+				int time = Integer.parseInt(com_parts[2]);
+				myOven.setTimer(time);
+			} else {
+				//throw an exception
+				throw new Exception("Invalid command");
+			}
 	}
-
 }
